f30df8470d8b5e4f0c2a0efe63934f9a3ac24524
Cittadhammo <82962905+cittadhammo@users.noreply.github.com>
2022-12-29 14:42:31 +0000

    test

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,7 @@ npm run build
 git add dist -f
 git commit -m "deploy"
 git subtree push --prefix dist origin gh-page
+
+
+git push origin `git subtree push --prefix dist origin gh-page`:production --force
+
