cde7fac63c2a870967e424023303425e19ac2a32
Cittadhammo <82962905+cittadhammo@users.noreply.github.com>
2022-12-29 14:24:54 +0000

    change to dynamic load

--- main.js
+++ main.js
@@ -5,69 +5,41 @@ import Projection from 'ol/proj/Projection';
 import Static from 'ol/source/ImageStatic';
 import View from 'ol/View';
 import {getCenter} from 'ol/extent';
-import {getHeight} from 'ol/extent';
-import {getWidth} from 'ol/extent';
-// import Extent from 'ol/extent';
 
-import Suttapitaka from './suttapitaka.svg'
-
-
-// Map views always need a projection.  Here we just want to map image
-// coordinates directly to map coordinates, so we create a projection that uses
-// the image extent in pixels.
-// const extent = [0, 0, 1, 1];
-// const projection = new Projection({
-//   code: 'xkcd-image',
-//   units: 'pixels',
-//   extent: extent,
-// });
-//
-// const map = new Map({
-//   layers: [
-//     new ImageLayer({
-//       source: new Static({
-//         attributions: '© <a href="https://xkcd.com/license.html">xkcd</a>',
-//         url: 'suttapitaka.svg',
-//         projection: projection,
-//         imageExtent: extent,
-//       }),
-//     }),
-//   ],
-//   target: 'map',
-//   view: new View({
-//     projection: projection,
-//     center: getCenter(extent),
-//     zoom: 2,
-//     maxZoom: 8,
-//   }),
-// });
-
-var extent = [0, 0, 3456/7, 5323/7];
-var projection = new Projection({
-  code: 'static-image',
-  units: 'pixels',
-  extent: extent
-});
-
-var map = new Map({
-  layers: [
-    new ImageLayer({
-      source: new Static({
-        url: Suttapitaka,
-        projection: projection,
+function dynamicMap (url) {
+  
+  const imageLayer = new ImageLayer();
+  const imageView = new View();
+  const img = document.createElement('img');
+  img.onload = function() {
+    const extent = [0, 0, img.width, img.height];
+    const projection = {
+      code: "xkcd-image" + url,
+      units: "pixels",
+      extent: extent,
+    }
+    imageLayer.setSource(
+      new Static({
+        url: url,
+        projection: new Projection(projection),
         imageExtent: extent,
-        imageLoadFunction: function (image, src) {
-           image.getImage().src = src;
-           image.getImage().width = getWidth(extent);
-           image.getImage().height = getHeight(extent);
-        }
-      })
+      }),
+    );
+    imageView.setProperties({
+      projection: projection,
+      center: getCenter(extent),
+      zoom: 2
     })
-  ],
-  target: 'map',
-  view: new View({
-    projection: projection,
-    center: getCenter(extent),
-    zoom: 2
-  })
-});
+  
+  };
+  img.src = url;
+  const map = new Map({
+    layers: [imageLayer],
+    target: 'map',
+    view: imageView
+  });
+  return map;
+}
+
+var map = new Map(dynamicMap("https://upload.wikimedia.org/wikipedia/commons/a/aa/Simple_shapes_example.png"));
+
